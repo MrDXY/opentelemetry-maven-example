205881facfcd525b85e4002bc3faa25f26865e9c
dxinyuan <xinyuandu@bupt.cn>
2023-09-01 10:56:32 +0800

    first commit

---
+++ src/main/java/com/xinyuan/opentelemetryexample/OpenTelemetryExampleApplication.java
@@ -0,0 +1,13 @@
+package com.xinyuan.opentelemetryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenTelemetryExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenTelemetryExampleApplication.class, args);
+    }
+
+}
