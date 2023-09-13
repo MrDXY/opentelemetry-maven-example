e1fc9d72e6a49dbab459bf8799f4c2ca94b67a21
dxinyuan <xinyuandu@bupt.cn>
2023-09-13 17:26:09 +0800

    enable prometheus

--- src/main/java/com/xinyuan/opentelemetryexample/config/OpenTelemetryConfiguration.java
+++ src/main/java/com/xinyuan/opentelemetryexample/config/OpenTelemetryConfiguration.java
@@ -4,14 +4,22 @@ import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
 import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.logs.OtlpGrpcLogRecordExporter;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.logs.SdkLoggerProvider;
+import io.opentelemetry.sdk.logs.export.BatchLogRecordProcessor;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
 import io.opentelemetry.sdk.trace.SdkTracerProvider;
 import io.opentelemetry.sdk.trace.export.SimpleSpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
 
 @Configuration
 public class OpenTelemetryConfiguration {
@@ -27,21 +35,24 @@ public class OpenTelemetryConfiguration {
                 .setResource(resource)
                 .build();
 
-//            SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
-//                    .registerMetricReader(PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().build()).build())
-//                    .setResource(resource)
-//                    .build();
-//
-//            SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
-//                    .addLogRecordProcessor(BatchLogRecordProcessor.builder(OtlpGrpcLogRecordExporter.builder().build()).build())
-//                    .setResource(resource)
-//                    .build();
+        SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
+                .registerMetricReader(
+                        PeriodicMetricReader.builder(OtlpGrpcMetricExporter.builder().build())
+                                .setInterval(Duration.ofSeconds(10))
+                                .build())
+                .setResource(resource)
+                .build();
+
+        SdkLoggerProvider sdkLoggerProvider = SdkLoggerProvider.builder()
+                .addLogRecordProcessor(BatchLogRecordProcessor.builder(OtlpGrpcLogRecordExporter.builder().build()).build())
+                .setResource(resource)
+                .build();
 
         return OpenTelemetrySdk.builder()
                 .setTracerProvider(sdkTracerProvider)
-//                    .setMeterProvider(sdkMeterProvider)
-//                    .setLoggerProvider(sdkLoggerProvider)
-                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+                .setMeterProvider(sdkMeterProvider)
+                .setLoggerProvider(sdkLoggerProvider)
+//                .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
                 .buildAndRegisterGlobal();
     }
 }
--- src/main/java/com/xinyuan/opentelemetryexample/controllers/PingController.java
+++ src/main/java/com/xinyuan/opentelemetryexample/controllers/PingController.java
@@ -1,8 +1,14 @@
 package com.xinyuan.opentelemetryexample.controllers;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.LongHistogram;
+import io.opentelemetry.api.metrics.Meter;
 import io.opentelemetry.api.trace.Span;
 import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -37,12 +43,36 @@ public class PingController {
             Span multiAttrSpan = tracer.spanBuilder("Example Span Attributes").startSpan();
             multiAttrSpan.setAttribute("Attribute 1", "first attribute value");
             multiAttrSpan.setAttribute("Attribute 2", "second attribute value");
-            multiAttrSpan.end();
             internalCall();
+            multiAttrSpan.end();
             return "pong"+id;
         } catch (Exception e) {
             return "exception occurred";
         }
+    }
+
+    @GetMapping("/test")
+    public String test() throws InterruptedException {
+        Tracer tracer = openTelemetry.getTracer("com.xinyuan.opentelemetryexample.controllers");
+        Meter meter = openTelemetry.getMeter("com.xinyuan.opentelemetryexample.controllers");
+        LongCounter counter = meter.counterBuilder("example.counter").build();
+        LongHistogram histogram = meter.histogramBuilder("super.timer").ofLongs().setUnit("ms").build();
+
+        long startTime = System.currentTimeMillis();
+        Span exampleSpan = tracer.spanBuilder("exampleSpan").startSpan();
+        Context exampleContext = Context.current().with(exampleSpan);
+        try (Scope scope = exampleContext.makeCurrent()) {
+            counter.add(1);
+            exampleSpan.setAttribute("good", true);
+            exampleSpan.setAttribute("exampleNumber", 1);
+            Thread.sleep(1000);
+        } finally {
+            histogram.record(
+                    System.currentTimeMillis() - startTime, Attributes.empty(), exampleContext);
+            exampleSpan.end();
+        }
+
+        return "end";
     }
 
     private void internalCall() throws InterruptedException {
